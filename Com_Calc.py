badd6857ceffa9005146406a93ef120b81e84c48
Bolo <107210022+boloanalytics@users.noreply.github.com>
2023-02-19 23:57:18 +0100

    Add files via upload

---
+++ Com_Calc.py
@@ -0,0 +1,5 @@
+name = input("Please tell me your name: ")
+sales = int(input("Please enter your total months sales: "))
+commission = round(sales * 13/100,2)
+print(f"Hello {name}, your commission this month is ${commission}")
+
